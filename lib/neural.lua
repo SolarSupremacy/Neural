49709dd4e8076028f919e3c7b989c37ab63798ce
SolarSupremacy <SolarSupremacyOfficial@gmail.com>
2017-07-23 17:49:54 -0400

    Automagic Recurrent Functionality
    Calling "makeGenome" now allows for recurrent matricies and other functions detect it properly.

--- lib/neural.lua
+++ lib/neural.lua
@@ -34,19 +34,29 @@ end
 
 function neural.process(inputs, genome)
   genome[2][1] = inputs
-  for i=1, #genome[2]-1 do
-    function act(x) return ((2.71828^(2*x)-1)/(2.71828^(2*x)+1)) end
-    for j=1, #genome[2][i] do
+  function act(x) return ((2.71828^(2*x)-1)/(2.71828^(2*x)+1)) end
+  
+  
+  for i=1, #genome[1] do
+    if (#genome[1][i]) == (#genome[2][i]) then
+      genome[2][i+1] = matrix.func(matrix.mul({genome[2][i]}, genome[1][i]), act)[1]
+    elseif (#genome[1][i]) == (#genome[2][i] + #genome[2][i+1]) then
+      genome[2][i+1] = matrix.func(matrix.mul({matrix.tableAdd(genome[2][i], genome[2][i+1])}, genome[1][i]), act)[1]
     end
-    genome[2][i+1] = matrix.func(matrix.mul({genome[2][i]}, genome[1][i]), act)[1] 
   end
+  
   return (genome)
 end
 
-function neural.makeGenome(setup)
+function neural.makeGenome(setup, genType)
+  genType = genType or "normal"
   local genome = {{}, {}}
   for i=1, (#setup-1) do
-    genome[1][i] = matrix.new(setup[i], setup[i+1], true, -2, 2)
+    if (genType == "recurrent") then
+      genome[1][i] = matrix.new(setup[i]+setup[i+1], setup[i+1], true, -2, 2)
+    else
+      genome[1][i] = matrix.new(setup[i], setup[i+1], true, -2, 2)
+    end
   end
   for i=1, #setup do
     local blanklist = {}
@@ -55,6 +65,7 @@ function neural.makeGenome(setup)
     end
     genome[2][i] = blanklist
   end
+  
   return (genome)
 end
 
