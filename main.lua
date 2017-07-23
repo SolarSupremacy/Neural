237ebbd1af81a9b11482d2910e4fe3d9c585fa05
SolarSupremacy <SolarSupremacyOfficial@gmail.com>
2017-07-23 12:18:08 -0400

    More Progress
    Added configuration capablility for LOVE and some changes to allow for future performance of 1000x or more before these changes.

--- main.lua
+++ main.lua
@@ -8,25 +8,80 @@ math.randomseed(os.time())
 local matrix = require("lib.matrix")
 local neural = require("lib.neural")
 
+function love.run()
+  
+	if love.math then
+		love.math.setRandomSeed(os.time())
+	end
+  
+	if love.load then love.load(arg) end
+  
+	if love.timer then love.timer.step() end
+  
+	local dt = 0
+  
+	while true do
+		-- Process events.
+		if love.event then
+			love.event.pump()
+			for name, a,b,c,d,e,f in love.event.poll() do
+				if name == "quit" then
+					if not love.quit or not love.quit() then
+						return a
+					end
+				end
+				love.handlers[name](a,b,c,d,e,f)
+			end
+		end
+    
+		if love.timer then
+			love.timer.step()
+			dt = love.timer.getDelta()
+		end
+		if love.update then love.update(dt) end
+ 
+		if love.graphics and love.graphics.isActive() then
+			love.graphics.clear(love.graphics.getBackgroundColor())
+			love.graphics.origin()
+			if love.draw then love.draw() end
+			love.graphics.present()
+		end
+ 
+		--if love.timer then love.timer.sleep(0.001) end
+	end
+ 
+end
+
 function love.load()
+  
+  timeWarp = 1
   testSetup = {2, 4, 3}
   testGenome = neural.makeGenome(testSetup)
+  draw = true
   
 end
 
-
 function love.update(dt)
-  -- Neural
-  testInput = {}
-  for i=1, testSetup[1] do
-    table.insert(testInput, math.random(-10, 10) / 10)
+  for i=1, timeWarp do
+    
+    -- Start Inputs, Neural Processing
+    testInput = {}
+    for i=1, testSetup[1] do
+      table.insert(testInput, math.random(-10, 10) / 10)
+    end
+    neural.process(testInput, testGenome)
+    
+    -- Start Outputs, Physics Processing
+    
+    
+    -- End Update
   end
-  neural.process(testInput, testGenome)
-  -- Physics
+  
 end
 
 function love.draw()
   love.graphics.print("FPS: " .. love.timer.getFPS(), 10, 10)
+  love.graphics.print("FPS: " .. 1/love.timer.getDelta(), 10, 20)
   
   netText = ""
   netText = netText .. table.concat(testGenome[2][1], " ") .. "\n\n"
@@ -36,6 +91,7 @@ function love.draw()
   end
   
   love.graphics.print("Neural Net: \n\n" .. netText, 10, 30)
-    
+  draw = false
+  
 end
 
